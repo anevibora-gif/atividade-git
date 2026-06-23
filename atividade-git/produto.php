f4a1aee8ac3bc2a952697dc3cb783a0d28d32217
Levi Fernandes <Levifernandes@gmail.com>
2026-06-23 11:34:45 -0300

    Inclusão da classe Produto

---
+++ atividade-git/produto.php
@@ -0,0 +1,11 @@
+<?php
+
+class Produto {
+    private $codigo;
+    private $descricao;
+
+    public function __construct($codigo, $descricao) {
+        $this->codigo = $codigo;
+        $this->descricao = $descricao;
+    }
+}
